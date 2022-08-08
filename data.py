d9c2139377c4e2035a9625b0f0516cacb9d21290
Nicholas Broad <nicholas@nmbroad.com>
2022-08-08 22:45:58 +0000

    truncate to max_length

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ from random import sample
 from dataclasses import dataclass
 
 import numpy as np
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from transformers import AutoTokenizer, DataCollatorWithPadding
 
 
@@ -35,30 +35,41 @@ class DataModule:
 
     def prepare_dataset(self):
 
-        num_train_examples = len(self.raw_dataset["train"])
+        if self.cfg["load_from_disk"]:
+            self.tokenized_dataset = load_from_disk(self.cfg["load_from_disk"])
+        else:
+            num_train_examples = len(self.raw_dataset["train"])
 
-        for i in sample(list(range(num_train_examples)), k=5):
-            x = self.raw_dataset["train"][i]
-            print(x["text"])
-            print([self.int2label[i] for i in x["labels"]])
+            for i in sample(list(range(num_train_examples)), k=5):
+                x = self.raw_dataset["train"][i]
+                print(x["text"])
+                print([self.int2label[i] for i in x["labels"]])
 
-        def tokenize(examples):
-            texts = ["".join(self.label_tokens + [t]) for t in examples["text"]]
-            tokenized = self.tokenizer(texts)
+            def tokenize(examples):
+                texts = ["".join(self.label_tokens + [t]) for t in examples["text"]]
+                tokenized = self.tokenizer(
+                    texts, 
+                    padding=False, 
+                    truncation=True,
+                    max_length=self.cfg["max_seq_length"],
+                )
 
-            num_examples = len(examples["labels"])
-            onehot_labels = np.zeros((num_examples, self.num_labels), dtype=np.float32)
+                num_examples = len(examples["labels"])
+                onehot_labels = np.zeros((num_examples, self.num_labels), dtype=np.float32)
 
-            for row_num, labels in enumerate(examples["labels"]):
-                onehot_labels[row_num, labels] = 1
+                for row_num, labels in enumerate(examples["labels"]):
+                    onehot_labels[row_num, labels] = 1
 
-            tokenized["labels"] = onehot_labels
+                tokenized["labels"] = onehot_labels
 
-            return tokenized
+                return tokenized
 
-        self.tokenized_dataset = self.raw_dataset.map(
-            tokenize, batched=True, num_proc=self.cfg["num_proc"]
-        )
+            self.tokenized_dataset = self.raw_dataset.map(
+                tokenize, batched=True, num_proc=self.cfg["num_proc"]
+            )
+            
+            
+            
 
 
 @dataclass
