73003b7b2d404a49526f8410fb52511b970a4638
Nicholas Broad <nicholas@nmbroad.com>
2022-08-08 22:57:52 +0000

    hyperparam search

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -33,6 +33,7 @@ logger = logging.getLogger(__name__)
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("config_file", type=str)
+    parser.add_argument("-l", "--load_from_disk", type=str, default=None)
 
     return parser
 
@@ -40,11 +41,15 @@ def main():
 
     parser = get_parser()
     args = parser.parse_args()
+    
 
     cfg, train_args = get_configs(args.config_file)
     set_wandb_env_vars(cfg)
 
     training_args = TrainingArguments(**train_args)
+    cfg["load_from_disk"] = args.load_from_disk
+    
+    
 
     # Setup logging
     logging.basicConfig(
@@ -70,16 +75,23 @@ def main():
 
     dataset = data_module.tokenized_dataset
 
-    config = AutoConfig.from_pretrained(
-        cfg["model_name_or_path"],
-        num_labels=len(data_module.int2label),
-        problem_type=cfg["problem_type"],
-    )
 
-    model = AutoModelForTokenSequenceClassification.from_pretrained(
-        cfg["model_name_or_path"],
-        config=config,
-    )
+    def model_init():
+        config = AutoConfig.from_pretrained(
+            cfg["model_name_or_path"],
+            num_labels=len(data_module.int2label),
+            problem_type=cfg["problem_type"],
+            return_dict=True,
+        )
+        model =  AutoModelForTokenSequenceClassification.from_pretrained(
+            cfg["model_name_or_path"],
+            config=config,
+        )
+    
+    
+        model.transformer.resize_token_embeddings(len(data_module.tokenizer))
+        
+        return model
 
     if cfg["problem_type"] == "multi_label_classification":
         data_collator = DataCollatorFloatLabels(data_module.tokenizer, pad_to_multiple_of=cfg["pad_multiple"])
@@ -90,7 +102,7 @@ def main():
 
     # Initialize our Trainer
     trainer = Trainer(
-        model=model,
+        model_init=model_init,
         args=training_args,
         train_dataset=dataset["train"],
         eval_dataset=dataset["validation"],
@@ -101,40 +113,27 @@ def main():
     )
 
     trainer.remove_callback(WandbCallback)
+    
+    def optuna_hp_space(trial):
+        return {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-6, 7e-5, log=True),
+            "num_train_epochs": trial.suggest_int("num_train_epochs", 1, 5),
+            "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [16, 24, 32]),
+            "weight_decay": trial.suggest_categorical("weight_decay", [0.0, 0.01, 0.009, 0.02]),
+            "adam_epsilon": trial.suggest_categorical("adam_epsilon", [1e-6, 1e-7, 1e-8]),
+        }
 
-    # Training
-    if training_args.do_train:
+    def optuna_objective(metrics):
+        return metrics["eval_f1"]
 
-        train_result = trainer.train()
-        metrics = train_result.metrics
-        metrics["train_samples"] = len(dataset["train"])
-
-        trainer.save_model()  # Saves the tokenizer too for easy upload
-
-        trainer.log_metrics("train", metrics)
-        trainer.save_metrics("train", metrics)
-        trainer.save_state()
-
-    if training_args.do_predict:
-        logger.info("*** Predict ***")
-
-        metrics = trainer.predict(dataset["test"]).metrics
-        metrics["eval_samples"] = len(dataset["test"])
-
-        trainer.log_metrics("test", metrics)
-        trainer.save_metrics("test", metrics)
-
-    kwargs = {
-        "finetuned_from": cfg["model_name_or_path"],
-        "tasks": "text-classification",
-        "language": "en",
-        "dataset_tags": cfg["dataset_tags"],
-    }
-
-    if training_args.push_to_hub:
-        trainer.push_to_hub(**kwargs)
-    else:
-        trainer.create_model_card(**kwargs)
+    # using same trainer as above
+    best_trial = trainer.hyperparameter_search(
+        direction="maximize",
+        backend="optuna",
+        n_trials=25,
+        hp_space=optuna_hp_space,
+        compute_objective=optuna_objective
+    )
 
 
 if __name__ == "__main__":
