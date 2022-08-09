d3d1db3ab179fefa1805ac0f4388e1040e56dc31
Nicholas Broad <nicholas@nmbroad.com>
2022-08-08 19:57:02 -0700

    set_embeddings
    can choose a way to initialize the new embeddings.

--- model.py
+++ model.py
@@ -96,6 +96,37 @@ class AutoModelForTokenSequenceClassification(PreTrainedModel):
             logits=logits,
         )
 
+    def set_embeddings(self, method="avg", tokenizer=None):
+        """
+        if method == "avg" this sets the new embeddings to the average of all of the old
+        embeddings.
+        if method == "similar", then the tokenizer needs to be passed. The average value of the 
+        label embeddings will be used for the new tokens. If a label is 'neutral', then the new 
+        embedding will be the same as `new_value` as shown below
+        label_ids = tokenizer("neutral", add_special_tokens=False).input_ids
+        new_value = self.model.transformer.word_embeddings.weight[label_ids].mean()
+        """
+
+        num_old_embeddings = self.model.transformer.word_embeddings.weight.data.size(0) - self.config.num_labels
+
+        if method == "avg":
+            avg_value = self.model.transformer.embeddings.word_embeddings.weight.data[:num_old_embeddings].mean(dim=0)
+
+            self.model.transformer.word_embeddings.weight.data[num_old_embeddings:] = avg_value
+        
+        elif method == "similar":
+
+            for label in self.config.label2int.keys():
+                # literal_ids are the ids for the tokens if the label were put through the tokenizer
+                # tokenizer("neutral")
+
+                literal_ids = tokenizer(label.lower(), add_special_tokens=False, return_tensors="pt").input_ids
+                avg_value = self.model.transformer.word_embeddings.weight.data[literal_ids].mean(0)
+
+                # new_id is the new token id that was added
+                new_id = tokenizer(f"[{label}]", add_special_tokens=False).input_ids
+                self.model.transformer.word_embeddings.weight[new_id] = avg_value
+
 
 @dataclass
 class SequenceClassificationOutput(ModelOutput):
